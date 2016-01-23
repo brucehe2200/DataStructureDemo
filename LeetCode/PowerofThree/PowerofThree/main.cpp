88ad0926512dc90009433b15f45a783634d620a9
helinlin <964464328@qq.com>
2016-01-23 14:36:02 +0800

    修改代码目录，添加leetcode算法代码目录 	new file:   LeetCode/PowerofThree/PowerofThree.xcodeproj/project.pbxproj         new file:   LeetCode/PowerofThree/PowerofThree.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   LeetCode/PowerofThree/PowerofThree.xcodeproj/project.xcworkspace/xcuserdata/helinlin.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   LeetCode/PowerofThree/PowerofThree.xcodeproj/xcuserdata/helinlin.xcuserdatad/xcdebugger/Breakpoints_v2.xcbkptlist 	new file:   LeetCode/PowerofThree/PowerofThree.xcodeproj/xcuserdata/helinlin.xcuserdatad/xcschemes/PowerofThree.xcscheme 	new file:   LeetCode/PowerofThree/PowerofThree.xcodeproj/xcuserdata/helinlin.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   LeetCode/PowerofThree/PowerofThree/main.cpp
    	deleted:    PowerofThree/PowerofThree.xcodeproj/project.pbxproj
    	deleted:    PowerofThree/PowerofThree.xcodeproj/project.xcworkspace/contents.xcworkspacedata
    	deleted:    PowerofThree/PowerofThree.xcodeproj/project.xcworkspace/xcuserdata/helinlin.xcuserdatad/UserInterfaceState.xcuserstate
    	deleted:    PowerofThree/PowerofThree.xcodeproj/xcuserdata/helinlin.xcuserdatad/xcdebugger/Breakpoints_v2.xcbkptlist
    	deleted:    PowerofThree/PowerofThree.xcodeproj/xcuserdata/helinlin.xcuserdatad/xcschemes/PowerofThree.xcscheme

---
+++ LeetCode/PowerofThree/PowerofThree/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  PowerofThree
+//
+//  Created by 何淋淋 on 16/1/10.
+//  Copyright © 2016年 何淋淋. All rights reserved.
+//
+
+#include <iostream>
+#include <math.h>
+//采用迭代的方式来计算一个数是否为3的幂，其中1为3的0次幂
+bool isPower(int inputVal)
+{
+    return fmod(log10(inputVal)/log10(3), 1)==0;
+};
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout<< isPower(1);
+    return 0;
+}
