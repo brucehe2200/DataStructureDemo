a580c5c9aaf6c6505364bcc8cd5b3ade1e28314d
helinlin <964464328@qq.com>
2016-04-20 22:04:44 +0800

    修改单链表，添加模板类型

--- 循环链表－约瑟夫问题/循环链表－约瑟夫问题/main.cpp
+++ 循环链表－约瑟夫问题/循环链表－约瑟夫问题/main.cpp
@@ -1,45 +1,50 @@
 //
 //  main.cpp
-//  循环链表－约瑟夫问题
+//  模版C++
 //
-//  Created by 何淋淋 on 16/4/18.
+//  Created by 何淋淋 on 16/4/19.
 //  Copyright © 2016年 何淋淋. All rights reserved.
 //
 
 #include <iostream>
 using namespace std;
+template <typename Type>
+
 struct Node{
-    int  data;
+    Type  data;
     Node* pNext;//  节点指针
 };
-typedef Node* LinkList;
+
+typedef Node<int> * LinkList;
 // 头插法创建单链表
+template <typename Type>
 LinkList LinkedListCreatH(int m)
 {
-    Node *head;
-    head = (Node *)malloc(sizeof(Node));   //申请头结点空间
+    Node<Type> *head;
+    head = (Node<Type> *)malloc(sizeof(Node<Type>));   //申请头结点空间
     head->pNext = NULL;                      //初始化一个空链表
     head->data = 0;
-    Node* temp = head;
-
-    Node *p;
+    Node<Type>* temp = head;
+    
+    Node<Type> *p;
     for (int i = 1;i <= m; i++) {
-        p = (Node*)malloc(sizeof(Node));
+        p = (Node<Type>*)malloc(sizeof(Node<Type>));
         p->data = i;
         temp->pNext = p;
         temp = temp->pNext;
     }
     temp->pNext = head->pNext;// 将指针指向头节点，形成循环链表
     // 删除头节点
-    Node* delNode = head;
+    Node<Type>* delNode = head;
     head = head->pNext;
     free(delNode);
     return head;
 }
 // 解决约瑟夫环的问题,从编号为K的元素开始，每逢m则出列
+template <typename Type>
 void print_link(LinkList L,int k,int m){
-    Node* ptemp = L;
-    Node* delNode = NULL;
+    Node<Type>* ptemp = L;
+    Node<Type>* delNode = NULL;
     for (int i = 0; i < k; i++) {
         ptemp = ptemp->pNext;
     }
@@ -60,7 +65,6 @@ void print_link(LinkList L,int k,int m){
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
-    LinkList L = LinkedListCreatH(5);
-    print_link(L,3,3);
+    LinkList L = LinkedListCreatH<int>(6);
     return 0;
 }
---
+++ 模版C++/模版C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  模版C++
+//
+//  Created by 何淋淋 on 16/4/19.
+//  Copyright © 2016年 何淋淋. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
