b850d9efa06fa42fb7b1dd5e3449a240aed63751
root <root@vultr.guest>
2017-04-15 15:31:23 +0800

    deleted:    ../LeetCode/PowerofThree/PowerofThree.xcodeproj/project.xcworkspace/xcuserdata/helinlin.xcuserdatad/UserInterfaceState.xcuserstate 	deleted:    ../LeetCode/PowerofThree/PowerofThree.xcodeproj/xcuserdata/helinlin.xcuserdatad/xcdebugger/Breakpoints_v2.xcbkptlist 	deleted:    ../LeetCode/PowerofThree/PowerofThree.xcodeproj/xcuserdata/helinlin.xcuserdatad/xcschemes/xcschememanagement.plist 	deleted:    ../LeetCode/PowerofTwo/PowerofTwo.xcodeproj/project.xcworkspace/xcuserdata/helinlin.xcuserdatad/UserInterfaceState.xcuserstate 	deleted:    ../LeetCode/PowerofTwo/PowerofTwo.xcodeproj/xcuserdata/helinlin.xcuserdatad/xcdebugger/Breakpoints_v2.xcbkptlist 	deleted:    ../LeetCode/PowerofTwo/PowerofTwo.xcodeproj/xcuserdata/helinlin.xcuserdatad/xcschemes/PowerofTwo.xcscheme 	deleted:    ../LeetCode/PowerofTwo/PowerofTwo.xcodeproj/xcuserdata/helinlin.xcuserdatad/xcschemes/xcschememanagement.plist modified the LambdaExample.cc

--- Lambda/LambdaExample.cc
+++ Lambda/LambdaExample.cc
@@ -3,9 +3,9 @@
 // the lambda is anonymous function
 using namespace std;
 int main(){
-	int i;
-	for(i =0;i < 10;i++){
-		cout<<[=](){ return i*i+1;}<<endl;
+    for(int i =0;i < 10;i++){
+	  auto func = [=](){return i*(i+1);};
+	  cout<<func()<<endl;
 	}
 	return 1;
 }
