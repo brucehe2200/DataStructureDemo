4e7a85d2b25446f3160ed7b31d44ad638f308d92
helinlin <helinlin@helinlins-MacBook-Pro.local>
2017-04-18 22:43:06 +0800

    add the implement code of MeddileValueOfSortArray

---
+++ LeetCode/MeddileValueOfSortArray/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    static double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> resultList;
+        int i=0,j=0;
+        int index = 0;
+        while(i<nums1.size()&&j<nums2.size()){
+            if(nums1[i] <= nums2[j]){
+                resultList.push_back(nums1[i]);
+                i++;
+            }
+            if(nums1[i] >= nums2[j]){
+                resultList.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while(i < nums1.size()){
+            resultList.push_back(nums1[i]);
+            i++;
+        }
+        while(j < nums2.size()){
+            resultList.push_back(nums2[j]);
+            j++;
+        }
+        if(resultList.empty()){ return 0.0;}
+        if(resultList.size()%2 == 0){
+            return (resultList[resultList.size()/2 - 1] + resultList[resultList.size()/2])/2.0;
+        }
+        return resultList[resultList.size()/2];
+    }
+};
+int main() {
+    vector<int> nums1,nums2;
+    nums1.push_back(1);
+    nums1.push_back(2);
+    nums1.push_back(3);
+    nums2.push_back(1);
+    nums2.push_back(3);
+    cout<<Solution::findMedianSortedArrays(nums1,nums2)<<endl;
+    return 0;
+}
