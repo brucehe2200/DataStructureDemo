2f206bd9e632fe9e5f2dc26948d482e87b6c7e94
master <helinlin007@gmail.com>
2017-04-25 14:40:41 +0800

    Update solution.cc

--- LeetCode/Permutations/solution.cc
+++ LeetCode/Permutations/solution.cc
@@ -25,7 +25,7 @@ void backtrack(vector<int>& nums, int beginIndex,vector<vector<int>> &result) {
       for(int i = 0;i < resultList.size();i++){
         for(int j = 0;j < resultList[i].size();j++){
           cout<<resultList[i][j];
-          if(j < resultList.size() -1){cout<<"-->"}
+          if(j < resultList.size() -1){cout<<"-->";}
         }
           cout<<endl;
       }
